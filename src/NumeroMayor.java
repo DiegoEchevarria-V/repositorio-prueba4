bb0e0a028e62c9776925d37a5119b1bc2ea33367
DiegoEchevarria-V <diego.echevarriaviera18@gmail.com>
2026-04-07 14:39:20 -0500

    se agrego una linea de mensaje mas

--- src/NumeroMayor.java
+++ src/NumeroMayor.java
@@ -1,33 +1,34 @@
+
 import java.util.Scanner;
 
 public class NumeroMayor {
 
     public static void main(String[] args) {
-        
+
         Scanner teclado = new Scanner(System.in);
-        
+
         int n1, n2, n3, mayor;
 
         System.out.println("Ingresa n1: ");
         n1 = teclado.nextInt();
-        
+
         System.out.println("Ingresa n2: ");
         n2 = teclado.nextInt();
-        
+
         System.out.println("Ingresa n3: ");
         n3 = teclado.nextInt();
-        
-        
-        if( n1 > n2 && n1 > n3){
+
+        if (n1 > n2 && n1 > n3) {
             mayor = n1;
-        }else{
-            if(n2 > n3){
+        } else {
+            if (n2 > n3) {
                 mayor = n2;
-            }else{
+            } else {
                 mayor = n3;
-            }            
+            }
         }
-        System.out.println("El mayor es: "+ mayor);
-        
+        System.out.println("El mayor es: " + mayor);
+        System.out.println("Hola a todos");
+
     }
 }
